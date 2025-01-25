4ab00b704577708a6720a39bb576affc79ef37a9
mjclair <michaelclair@students.abtech.edu>
2025-01-25 08:56:18 -0500

    Add files via upload

---
+++ 2.2 Simple-Messges.py
@@ -0,0 +1,13 @@
+Python 3.13.1 (v3.13.1:06714517797, Dec  3 2024, 14:00:22) [Clang 15.0.0 (clang-1500.3.9.4)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # Assign a message to a variable
+... message = "Hello, welcome to Python programming!"
+>>> # Print the first message
+... print(message)
+Hello, welcome to Python programming!
+>>> 
+... # Change the value of the variable
+... message = "Here is my new message, testing testing!"
+>>> # Print the new message
+... print(message)
+Here is my new message, testing testing!
